97bee4fa443fd231bf6cd2448c3283bd687d7ba2
IdanLnrd <idan@lnrd.io>
2021-07-12 11:49:41 +0300

    api service

--- client/src/app/api/api.service.ts
+++ client/src/app/api/api.service.ts
@@ -1,16 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 const BASE_URL = 'https://nubela.co/proxycurl/api/linkedin/company';
 
-const Authorization = { Bearer: '' }
+const Authorization = { Bearer: '8048c8d1-b607-4400-9f58-c056e247879e' }
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ApiService {
-
-  constructor() { }
+  private headers = { 'Authorization': `Bearer ${Authorization.Bearer}` };
+  constructor(private http: HttpClient) { 
+  }
   // Linkedin Company Profile General Resolution Endpoint
-  // Linkedin Company Profile Endpoint
+  //    https://nubela.co/proxycurl/api/linkedin/company/resolve?location=sg&company_domain=nubela.co&company_name=Nubela
 
+  getCompanyLinkedInUrl(data:{ domain: string, name: string }) {
+    const { domain, name } = data;
+    const query = `company_domain=${domain}&company_name=${name}`;
+    return this.http.get(`${BASE_URL}/resolve?${query}`, { headers: this.headers });
+  }
+  // Linkedin Company Profile Endpoint
+  getCompanyLinkedInProfile(data: { linkedInUrl: string }) {
+    const { linkedInUrl } = data;
+    const query = `url=${linkedInUrl}`;
+    return this.http.get(`${BASE_URL}?${query}`, { headers: this.headers });
+  }
 }
