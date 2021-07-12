58625fb1930f16eac9e9573d0aebf0c858cb05f8
IdanLnrd <idan@lnrd.io>
2021-07-12 11:31:09 +0300

    init

---
+++ client/src/app/api/api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+const BASE_URL = 'https://nubela.co/proxycurl/api/linkedin/company';
+
+const Authorization = { Bearer: '' }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+  // Linkedin Company Profile General Resolution Endpoint
+  // Linkedin Company Profile Endpoint
+
+}
