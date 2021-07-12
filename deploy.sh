fdefe2fd7d0fdad9bd4fe35b259025c350529831
IdanLnrd <idan@lnrd.io>
2021-07-12 12:46:04 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 mkdir -p docs
+echo "Build client start"
+cd client
+ng build --prod
+cd ..
+echo "Build client done"
+echo "Copy dist directory"
+cp -R ./client/dist/client ./docs
+echo "Copy done"
+echo "Push repository"
 git add .
 git commit -m "deploy"
 git push origin main
